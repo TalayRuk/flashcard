0cbd472d27b8ead6eb99d794e26ed50f1b6d8a4b
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-09 16:19:56 -0700

    flashcard starting

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function(){
+  $(".clickable1").click(function() {
+    $(".showing1").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".showing2").toggle();
+  });
+  $(".clickable3").click(function() {
+    $(".showing3").toggle();
+  });
+  $(".clickable4").click(function() {
+    $(".showing4").toggle();
+  });
+  $(".clickable5").click(function() {
+    $(".showing5").toggle();
+  });
+  $(".clickable6").click(function() {
+    $(".showing6").toggle();
+  });
+});
