9fd6c3bb9e5cb8e29e04495ab15713e06be699dd
Talayruk <notejde@gmail.com>
2016-08-15 16:56:22 -0700

    added loop for clickable phrases

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
 $(function(){
-  $(".clickable1").click(function() {
-    $(".showing1").toggle();
+//     $(".showing1").toggle();
+//   });
+//   $(".clickable2").click(function() {
+//     $(".showing2").toggle();
+//   });
+//   $(".clickable3").click(function() {
+//     $(".showing3").toggle();
+//   });
+//   $(".clickable4").click(function() {
+//     $(".showing4").toggle();
+//   });
+//   $(".clickable5").click(function() {
+//     $(".showing5").toggle();
+//   });
+//   $(".clickable6").click(function() {
+//     $(".showing6").toggle();
+//   });
+// });
+
+  $(".clickable").click(function() {
+    var phrases = ['clickable1', 'clickable2', 'clickable3', 'clickable4', 'clickable5', 'clickable6'];
+
+    phrases.forEach(function(phrase) {
+     var show = $("." + phrase).click();
+     $("#" + phrase).toggle(show);
+
+
+     });
   });
-  $(".clickable2").click(function() {
-    $(".showing2").toggle();
-  });
-  $(".clickable3").click(function() {
-    $(".showing3").toggle();
-  });
-  $(".clickable4").click(function() {
-    $(".showing4").toggle();
-  });
-  $(".clickable5").click(function() {
-    $(".showing5").toggle();
-  });
-  $(".clickable6").click(function() {
-    $(".showing6").toggle();
-  });
+
 });
