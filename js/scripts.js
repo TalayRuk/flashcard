a1d357098ce25e1e2185f56d2609dff132099cb0
Talayruk <notejde@gmail.com>
2016-08-15 17:57:05 -0700

    rewrote & works but need to figure out to use child instead of id

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 $(function(){
-  $(".clickable").click(function(event) {
+
 
     var phrases = ['clickable1', 'clickable2', 'clickable3', 'clickable4', 'clickable5', 'clickable6'];
-    console.log($(".clickable1").text());
-
+    // console.log($("#clickable2").text());
     phrases.forEach(function(phrase){
-     var show = $("." + phrase).text();
-     alert(show);
-     $("#" + phrase).toggle(show);
+      $("." + phrase).click(function() {
+        // $(this).children().toggle();
+        $(this).children().toggle();
 
+      });
 
-    });
-  event.preventDefault();
+  //
+  //   phrases.forEach(function(phrase){
+  //    $("#" + phrase).show();
+    // $("#" + phrase).toggle(); ...replace line 9 works
+  //
+  //
+  //   });
+  // // event.preventDefault();
   });
 
 });
