369a3c98d4f3e2b82d74f86f7e86fd8d34d59cbe
Talayruk <notejde@gmail.com>
2016-08-15 17:13:11 -0700

    replaced .val w/.text>>text now showed in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,17 @@
 $(function(){
-//     $(".showing1").toggle();
-//   });
-//   $(".clickable2").click(function() {
-//     $(".showing2").toggle();
-//   });
-//   $(".clickable3").click(function() {
-//     $(".showing3").toggle();
-//   });
-//   $(".clickable4").click(function() {
-//     $(".showing4").toggle();
-//   });
-//   $(".clickable5").click(function() {
-//     $(".showing5").toggle();
-//   });
-//   $(".clickable6").click(function() {
-//     $(".showing6").toggle();
-//   });
-// });
+  $(".clickable").click(function(event) {
 
-  $(".clickable").click(function() {
     var phrases = ['clickable1', 'clickable2', 'clickable3', 'clickable4', 'clickable5', 'clickable6'];
+    console.log($(".clickable1").text());
 
-    phrases.forEach(function(phrase) {
-     var show = $("." + phrase).click();
+    phrases.forEach(function(phrase){
+     var show = $("." + phrase).text();
+     alert(show);
      $("#" + phrase).toggle(show);
 
 
-     });
+    });
+  event.preventDefault();
   });
 
 });
